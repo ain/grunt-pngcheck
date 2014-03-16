49e8c885a5bf2c30044d18425a115173c42a5193
Ain Tohvri <ain@flashbit.net>
2014-03-16 03:33:09 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+/*
+ * grunt-pngcheck
+ * https://github.com/ain/grunt-pngcheck
+ *
+ * Copyright (c) 2014 Ain Tohvri
+ * Licensed under the GPL license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+  // load all npm grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    pngcheck: {
+      success: {
+        files: {
+          src: ['test/fixtures/images/*.png', '!test/fixtures/images/corrupt.png']
+        }
+      },
+      failure: {
+        files: {
+          src: ['test/fixtures/images/*.png']
+        }
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'pngcheck', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/pngcheck.js
@@ -0,0 +1,48 @@
+/*
+ * grunt-pngcheck
+ * https://github.com/ain/grunt-pngcheck
+ *
+ * Copyright (c) 2014 Ain Tohvri
+ * Licensed under the GPL license.
+ */
+
+'use strict';
+var PNG = require('png-js');
+var fs = require('fs');
+
+module.exports = function (grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('pngcheck', 'Test PNG files against corruption.', function () {
+
+    var options = this.options({
+      punctuation: '. !!!',
+      separator: ',: '
+    });
+
+    this.files.forEach(function (file) {
+      // Concat specified files.
+      var src = file.src.filter(function (filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function (filepath) {
+        try {
+          PNG.load(filepath);
+          grunt.log.ok(filepath);
+        }
+        catch (error) {
+          grunt.fail.fatal(error.message + ': ' + filepath);
+        }
+      });
+      grunt.log.ok('Check successful: no PNGs corrupt.');
+    });
+  });
+
+};
