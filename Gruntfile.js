cf77269e54a93e7b15e9c47958198d5e0d618370
Ain Tohvri <at@interactive-pioneers.de>
2014-03-30 13:30:40 +0200

    Add JIT Grunt for task loading

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,8 +9,7 @@
 'use strict';
 
 module.exports = function (grunt) {
-  // load all npm grunt tasks
-  require('load-grunt-tasks')(grunt);
+  require('jit-grunt')(grunt);
 
   // Project configuration.
   grunt.initConfig({
